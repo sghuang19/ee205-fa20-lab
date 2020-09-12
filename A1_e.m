7f01ffcbfbcc3adb1174922d079e5ffcf78b774f
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-12 19:52:13 +0800

    Create A1_e.m

---
+++ A1_e.m
@@ -0,0 +1,23 @@
+% define scope
+l = 5;
+
+%1.4 advanced problem e
+%linearity
+n = [-l:l];
+x1 = n;
+x2 = 2 * n;
+y1 = x1.^3;
+y2 = x2.^3;
+y3 = 2 * y1;
+stem(n, y1);
+hold on
+stem(n, y2);
+hold on
+stem(n, y3);
+
+title('y[n] = x^3[n]');
+xlabel('Time');
+ylabel('Output');
+legend('y_1[n] = x^3[n]', ...
+    'y_2[n] = (2x[n])^3', ...
+    'y_3[n] = 2x^3[n]')
