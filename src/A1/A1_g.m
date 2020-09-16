61866b22b077d10258c9bf94a0815f453ad8845f
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-16 19:45:39 +0800

    UPDATE

--- src/A1/A1_g.m
+++ src/A1/A1_g.m
@@ -1,22 +1,24 @@
 % define scope
-l = 5;
+l = 10;
 
 %1.4 advanced problem g
+%linearity
 n = [-l:l];
-x1 = n;
-x2 = 2 * n;
-y1 = x1.^3;
-y2 = x2.^3;
-y3 = 2 * y1;
-stem(n, y1);
-hold on
-stem(n, y2);
-hold on
-stem(n, y3);
 
-title('y[n] = x^3[n]');
+% define x[n] = n^2
+x1 = 4 * n.^2; % x[2n] = (2n)^2 = 4n^2
+x2 = 16 * n.^2; % x[2(2n)] = (4n)^2 = 16n^2
+x3 = 8 * n.^2 % 2x[2n] = 8n^2
+
+stem(n, x1);
+hold on
+stem(n, x2);
+hold on
+stem(n, x3)
+
+title('y[n] = x[2n]');
 xlabel('Time');
 ylabel('Output');
-legend('y_1[n] = x^3[n]', ...
-    'y_2[n] = (2x[n])^3', ...
-    'y_3[n] = 2x^3[n]')
+legend('y_1[n] = x[2n]', ...
+    'y_2[n] = x[2(2n)]', ...
+    'y_3[n] = 2x[2n]')
