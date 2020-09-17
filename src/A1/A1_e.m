b472f5e2b6857dc099887645ce89fd001c2cae48
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-17 13:24:41 +0800

    Update A1_e.m

--- src/A1/A1_e.m
+++ src/A1/A1_e.m
@@ -1,14 +1,14 @@
 % define scope
 l = 5;
 
-%1.4 advanced problem e
-%linearity
+% 1.4 Advanced Problem e
 n = [-l:l];
-x1 = n;
-x2 = 2 * n;
-y1 = x1.^3;
-y2 = x2.^3;
-y3 = 2 * y1;
+x = n;
+
+y1 = L(x);
+y2 = L(x + x)
+y3 = y1 + y1
+
 stem(n, y1);
 hold on
 stem(n, y2);
@@ -19,5 +19,10 @@ title('y[n] = x^3[n]');
 xlabel('Time');
 ylabel('Output');
 legend('y_1[n] = x^3[n]', ...
-    'y_2[n] = (2x[n])^3', ...
-    'y_3[n] = 2x^3[n]')
+    'y_2[n] = (x[n] + x[n])^3', ...
+    'y_3[n] = y_1[n] + y_1[n]')
+
+% define system
+function y = L(x)
+    y = x.^3;
+end
