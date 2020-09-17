3f1eb00d634baee4e6f91d26d9d9503fe315181e
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-17 13:24:27 +0800

    Update A1_a.m

--- src/A1/A1_a.m
+++ src/A1/A1_a.m
@@ -1,16 +1,27 @@
-% Define scopes
-l = 2;
+% define scopes
+l = 5;
 
 % Basic Problem a
-n = [-l : l];
+n = [-l:l];
 x1 = [zeros(1, l) 1 zeros(1, l)];
-x2 = 2*x1;
-y1 = sin(pi/2*x1);
-y2 = sin(pi/2*x2);
-stem(n, y1, 'o')
+x2 = 2 * x1;
+x3 = x1 + x2;
+
+y1 = L(x1);
+y2 = L(x2);
+y3 = y1 + y2
+y4 = L(x3)
+
+stem(n, y3, 'o')
 hold on
-stem(n, y2, '*')
+stem(n, y4, '*')
 title('y[n] = sin((pi/2)x[n])')
 xlabel('Time');
-ylabel('y-value')
-legend('y(x_1[n])', 'y(x_2[n])');
+ylabel('Output')
+legend('y_1[n] = sin((pi/2)x_1[n]) + sin((pi/2)x_2[n])', ...
+    'y_2[n] = sin((pi/2)(x_1[n]+x_2[n]))');
+
+% define system
+function y = L(x)
+    y = sin(pi / 2 * x)
+end
