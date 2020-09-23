85c10f356775825fdb61a2339f4281fd3b3152e0
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-23 20:20:06 +0800

    Update A1_g.m

--- src/A1/A1_g.m
+++ src/A1/A1_g.m
@@ -3,25 +3,50 @@ l = 10;
 
 % 1.4 advanced problem g
 % time-invariance
-% def
 
 n = [-l:l]
-t = 5
-nt = n - t
-x1 = n
-x2 = nt
-y1 = 2 * x1
-y2 = 2 * x2
+t = 2;
+y1 = L(n)
+nt = n - t;
+y2 = [zeros(1, t) y1(1, t + 1:2 * l + 1)]
+y3 = L(nt)
 
-stem(n, y1);
+figure(1);
+stem(n, y1, '*')
 hold on
-stem(n, y2);
+stem(n, y2)
 hold on
-stem(nt, y1);
+stem(n, y3, '*')
 
-title('y[n] = x[2n]');
-xlabel('Time');
-ylabel('Output');
+title('y[n] = x[2n]')
+xlabel('Time')
+ylabel('Output')
 legend('y_1[n] = x[2n]', ...
-    'y_2[n] = x[2(2n)]', ...
-    'y_3[n] = 2x[2n]')
+    'y_2[n] = x[2n - t]', ...
+    'y_3[n] = y_1[n-t] = x[2(n - t)]')
+
+%causality
+
+l = 4;
+n = [-l:l];
+% define unit impulse input at n = 2
+x1 = [zeros(1, l + 2) 1 zeros(1, l - 2)]
+
+nt = 1:2:2 * l + 1
+y4 = [zeros(1, l / 2) x1(nt) zeros(1, l / 2)]
+
+figure(2);
+stem(n, x1, '*');
+hold on
+stem(n, y4)
+
+title('y[n] = x[2n]')
+xlabel('Time')
+ylabel('Output')
+legend('x[n] = \delta[n - 1]', ...
+    'y[n] = x[2n]')
+
+%def function
+function y = L(x)
+    y = x.^2
+end
