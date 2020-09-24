253344245e1dc795f6fe7e60bb6d7638d0be4e30
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 10:00:17 +0800

    Update A1_1_5_a.m

--- src/A1/A1_1_5_a.m
+++ src/A1/A1_1_5_a.m
@@ -1,3 +1,4 @@
+% 1.5 Advanced Problem a
 % diffeqn(1, [1 1 1 1 1], 1)
 
 function y = diffeqn(a, x, yn1)
@@ -5,7 +6,7 @@ function y = diffeqn(a, x, yn1)
     y(1) = a * yn1 + x(1)
 
     for index = 2:N
-        y(index) = a * y(index - 1) + x(index)
+        y(index) = a * y(index - 1) + x(index);
     end
 
 end
