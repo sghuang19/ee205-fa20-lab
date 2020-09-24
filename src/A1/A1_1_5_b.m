78861ca8aaac0b2dc4e11b8648b2e76efacc1556
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 08:27:19 +0800

    Update A1_1_5_b.m

--- src/A1/A1_1_5_b.m
+++ src/A1/A1_1_5_b.m
@@ -1,11 +1,18 @@
-% diffeqn(1, [1 1 1 1 1], 1)
+a = 1;
+yn1 = 0;
+n = 0:1:30;
+x1 = [1 zeros(1, 30)];
+x2 = ones(1, 31);
 
-function y = diffeqn(a, x, yn1)
-    N = length(x)
-    y(1) = a * yn1 + x(1)
+y1 = A1_1_5_a(a, x1, yn1)
+y2 = A1_1_5_a(a, x2, yn1)
 
-    for index = 2:N
-        y(index) = a * y(index - 1)
-    end
+stem(n, y1, '*')
+hold on
+stem(n, y2)
 
-end
+title('y[n] = ay[n-1] + x[n]')
+xlabel('Time')
+ylabel('Output')
+legend('y_1[n] = y_1[n - 1] + \delta[n]', ...
+    'y_2[n] = y_2[n - 1] + u[n]')
