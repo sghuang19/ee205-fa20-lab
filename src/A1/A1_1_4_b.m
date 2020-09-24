d361c34e29bdbd0762c99bdf116db806587ad723
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 22:09:54 +0800

    Update A1_1_4_b.m

--- src/A1/A1_1_4_b.m
+++ src/A1/A1_1_4_b.m
@@ -5,10 +5,11 @@ x0 = [zeros(1, 6) 1/2 ones(1, 9)];
 x1 = [zeros(1, 5) 1/2 ones(1, 10)];
 y = x0 + x1;
 
-stem(n, x, '*');
+stem(n, x, '*')
 hold on
-stem(n, y);
+stem(n, y)
 
 title('y[n] = x[n] + x[n + 1]')
 xlabel('Time')
-legend('x[n] = u[n]', 'y[n] = x[n] + x[n + 1]')
+legend('x[n] = u[n]', ...
+    'y[n] = x[n] + x[n + 1]')
