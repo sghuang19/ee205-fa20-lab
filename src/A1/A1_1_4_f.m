c6744315e488c3d9ef4d2cf956db66ef87df0c27
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 19:02:22 +0800

    Update A1_1_4_f.m

--- src/A1/A1_1_4_f.m
+++ src/A1/A1_1_4_f.m
@@ -1,13 +1,14 @@
+% define scope
 l1 = 5;
 l2 = l1 -1;
 
-% for time invariant and satble and invertible
+% time-invariance
 % the original function:x[n] = cos(n) and y = n*x[n]
 n1 = [-l1:l1];
 x1 = cos(n1);
 y1 = n1 .* x1;
 
-% let the input signal have a time shift:x2[n] = x1[n-1]
+% let the input signal have a time shift: x2[n] = x1[n-1]
 n2 = [-l2:l2];
 x2 = [0 cos(n2) 0];
 y2 = n1 .* x2;
@@ -16,6 +17,19 @@ stem(n1, y1, '*');
 hold on
 stem(n1, y2);
 
+
+
+function output = myFun(input)
+    %myFun - Description
+    %
+    % Syntax: output = myFun(input)
+    %
+    % Long description
+
+end
+
 title('y = n*x[n]');
 xlabel('Time');
 ylabel('Output');
+
+% stability and invertible
