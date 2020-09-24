7ace160b55e02c409a6083b727716c29602b7da7
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 22:10:00 +0800

    Update A1_1_4_f.m

--- src/A1/A1_1_4_f.m
+++ src/A1/A1_1_4_f.m
@@ -4,25 +4,29 @@ l = 10;
 
 % time-invariance
 % the original function:x[n] = cos(n) and y = n*x[n]
-n1 = [-l:l];
+n1 = -l:l;
 x1 = cos(n1);
 y1 = n1 .* x1
 
 % let the input signal have a time shift: x2[n] = x1[n-1]
 n2 = n1 - 1;
-y2 = n1 .* cos(n2)
+x2 = cos(n2);
+y2 = n1 .* x2
 
 % let the original output signal have a time shift: y3[n] = y1[n-1]
-y3 = n2 .* cos(n2)
+y3 = n2 .* x2
 
-stem(n1, y1, '*')
+stem(n1, y1, '*g')
 hold on
-stem(n1, y2)
+stem(n1, y2, '*')
 hold on
-plot(n1, y3, '--')
+stem(n1, y3, 'o')
 
-title('y = nx[n]');
-xlabel('Time');
-ylabel('Output');
+title('y = nx[n]')
+xlabel('Time')
+ylabel('Output')
+legend('y_1[n] = nx[n]', ...
+    'y_1[n] = nx[n - 1]', ...
+    'y_3[n] = (n - 1)x[n - 1]')
 
 % stability and invertible
