525263da6f556739e291617547f806359f0b3c2f
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 21:11:27 +0800

    Update A1_1_4_f.m

--- src/A1/A1_1_4_f.m
+++ src/A1/A1_1_4_f.m
@@ -1,34 +1,27 @@
+clf
 % define scope
-l1 = 5;
-l2 = l1 -1;
+l = 10;
 
 % time-invariance
 % the original function:x[n] = cos(n) and y = n*x[n]
-n1 = [-l1:l1];
+n1 = [-l:l];
 x1 = cos(n1);
-y1 = n1 .* x1;
+y1 = n1 .* x1
 
 % let the input signal have a time shift: x2[n] = x1[n-1]
-n2 = [-l2:l2];
-x2 = [0 cos(n2) 0];
-y2 = n1 .* x2;
+n2 = n1 - 1;
+y2 = n1 .* cos(n2)
 
-stem(n1, y1, '*');
+% let the original output signal have a time shift: y3[n] = y1[n-1]
+y3 = n2 .* cos(n2)
+
+stem(n1, y1, '*')
 hold on
-stem(n1, y2);
+stem(n1, y2)
+hold on
+plot(n1, y3, '--')
 
-
-
-function output = myFun(input)
-    %myFun - Description
-    %
-    % Syntax: output = myFun(input)
-    %
-    % Long description
-
-end
-
-title('y = n*x[n]');
+title('y = nx[n]');
 xlabel('Time');
 ylabel('Output');
 
