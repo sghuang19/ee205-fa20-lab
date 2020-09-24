2de2b5ac25af979074106826e874993c1e2efdd5
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-09-24 09:03:41 +0800

    Update A1_1_4_f.m

--- src/A1/A1_1_4_f.m
+++ src/A1/A1_1_4_f.m
@@ -5,15 +5,15 @@ l2 = l1 -1;
 %the origin function:x[n] = cos(n) and y = n*x[n]
 n1 = [-l1:l1];
 x1 = cos(n1);
-y1 = n1.* x1;
+y1 = n1 .* x1;
 %let the input signal have a time shift:x2[n] = x1[n-1]
 n2 = [-l2:l2];
 x2 = [0 cos(n2) 0];
-y2 = n1.* x2;
+y2 = n1 .* x2;
 
-stem(n1,y1,'*');
+stem(n1, y1, '*');
 hold on
-stem(n1,y2);
+stem(n1, y2);
 
 title('y = n*x[n]');
 xlabel('Time');
