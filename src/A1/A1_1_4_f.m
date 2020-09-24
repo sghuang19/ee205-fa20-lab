640d38b3c43e643320fb53d3df3f0883c673946c
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-09-24 10:44:59 +0800

    Update A1_1_4_f.m

--- src/A1/A1_1_4_f.m
+++ src/A1/A1_1_4_f.m
@@ -1,12 +1,13 @@
 l1 = 5;
 l2 = l1 -1;
 
-%for time invariant and satble and invertible
-%the origin function:x[n] = cos(n) and y = n*x[n]
+% for time invariant and satble and invertible
+% the original function:x[n] = cos(n) and y = n*x[n]
 n1 = [-l1:l1];
 x1 = cos(n1);
 y1 = n1 .* x1;
-%let the input signal have a time shift:x2[n] = x1[n-1]
+
+% let the input signal have a time shift:x2[n] = x1[n-1]
 n2 = [-l2:l2];
 x2 = [0 cos(n2) 0];
 y2 = n1 .* x2;
