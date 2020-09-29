834f2fc08206ccd664db40b49717ad033f572ab6
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-09-29 17:10:04 +0800

    Update A1_1_4_d.m

--- src/A1/A1_1_4_d.m
+++ src/A1/A1_1_4_d.m
@@ -3,10 +3,10 @@ l = 10;
 n = -l:l;
 x = n;
 
-y = sin((pi / 2) * x)
+y = sin((pi / 2) * x);
 
-stem(n, y, '*');
-title('y[n] = sin(pi/2 * x[n])');
+stem(n, y, '*')
+title('y[n] = sin(\pi/2 * x[n])')
 xlabel('Time');
 ylabel('Output');
 legend('y[n] = sin(pi/2 * x)');
