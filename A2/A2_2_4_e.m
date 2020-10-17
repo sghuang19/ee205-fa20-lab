6074a7bab2078a1f44a072491ca00dc628227feb
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-17 17:27:35 +0800

    Create A2_2_4_e.m

---
+++ A2/A2_2_4_e.m
@@ -0,0 +1,16 @@
+nx1 = 0:9;
+nh1 = 0:4;
+nh2 = 2:6;
+x1 = [ones(1,5) zeros(1,5)];
+h1 = [1 -1 3 0 1];
+h2 = h1;
+
+ny1 = nx1(1)+nh1(1):nx1(end)+nh1(end);
+ny2 = nx1(1)+nh2(1):nx1(end)+nh2(end)
+y1 = conv(x1,h1);
+y2 = conv(x1,h2);
+
+subplot(2,1,1),stem(ny1,y1),axis([0,15,ylim])
+xlabel('n');ylabel('ye1 = x1[n]*he1[n]');title('ye1');
+subplot(2,1,2),stem(ny2,y2),axis([0,15,ylim])
+xlabel('n');ylabel('ye2 = x1[n]*he2[n]');title('ye2');
