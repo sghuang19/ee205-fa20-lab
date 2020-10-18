96152c13b01ce942f0f0b82d3c7d734144dfef98
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-19 00:56:08 +0800

    Update A2_2_10_a.m

--- src/A2/A2_2_10_a.m
+++ src/A2/A2_2_10_a.m
@@ -3,7 +3,7 @@ x = [1 zeros(1,1000)];
 a1 = 1;
 b1 = [1 zeros(1,999) 0.5];
 
-y1 = filter(b1,a1,x);
+he = filter(b1,a1,x);
 
-stem(n, y1);
-xlabel('n');ylabel('y1');title('y[n] = x[n] + 0.5x[n-1000]');
+stem(n, he);
+xlabel('n');ylabel('Output');title('impulse response : he');
