555ff742f82ea4e0dbc9839e1a5ef62e99c80099
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-18 22:16:54 +0800

    Update A2_2_4_g.m

--- src/A2/A2_2_4_g.m
+++ src/A2/A2_2_4_g.m
@@ -4,7 +4,7 @@ hg2 = [0 2 5 4 -1];
 xg = 2.*x;
 
 yga = xg.^2;
-ygb = conv(yga,hg2);
+ygb = conv(xg,hg2);
 nygb = 0:8;
 yg1 = [yga zeros(1,4)] + ygb;
 nyg1 = 0:8;
@@ -15,6 +15,6 @@ yg2 = conv(xg,hparallel);
 nyg2 = 0:8;
 
 subplot(2,1,1),stem(nyg1,yg1),axis([0,8,ylim])
-xlabel('n');ylabel('yg1 = yga + ygb');title('yg1');
+xlabel('n');ylabel('yg1 = xg*hga + xg*hgb');title('yg1');
 subplot(2,1,2),stem(nyg2,yg2),axis([0,8,ylim])
 xlabel('n');ylabel('yf2 = xg*(hg1 + hg2)');title('yg2');
