560ba3dc708f72d1a3d4d94e5dc58c5798e257a1
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-21 23:14:57 +0800

    Update A2_2_5_f.m

--- src/A2/A2_2_5_f.m
+++ src/A2/A2_2_5_f.m
@@ -1,30 +1,30 @@
 n = 0:19;
 x = [1 zeros(1, 19)];
-x1 = ones(1,20);
+x1 = ones(1, 20);
 %System1
 a1 = [1 -0.6];
 b1 = [1 0];
-h1 = filter(b1,a1,x);
+h1 = filter(b1, a1, x);
 %System2
-h2 = zeros(1,20);
-h2(1) = 1;%cuz h2(i) = 0 if i <1, h2[0] = x[0] = 1, h2[1] = h2[0] + 0;
-for i= 1:19
-    h2(i+1) = 0.6^i*h2(i) +x(i+1);
+h2 = zeros(1, 20);
+h2(1) = 1; %cuz h2(i) = 0 if i <1, h2[0] = x[0] = 1, h2[1] = h2[0] + 0;
+
+for i = 1:19
+    h2(i + 1) = 0.6^i * h2(i) +x(i + 1);
 end
 
-z1 = conv(h1,x1);
+z1 = conv(h1, x1);
 nz1 = 0:38;
-z2 = conv(h2,x1);
+z2 = conv(h2, x1);
 nz2 = 0:38;
 
-subplot(2,1,1)
-stem(nz1,z1),axis([0,38,ylim])
+subplot(2, 1, 1)
+stem(nz1, z1), axis([0, 38, ylim])
 xlabel('n');
 ylabel('z1');
 title('z1[n] = h1[n]*x[n]');
-subplot(2,1,2)
-stem(nz2,z2),axis([0,38,ylim])
+subplot(2, 1, 2)
+stem(nz2, z2), axis([0, 38, ylim])
 xlabel('n');
 ylabel('z2');
 title('z2[n] = h2[n]*x[n]');
-
