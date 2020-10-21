df9e2706fde2f69feb8081654caa54cc7763bbe7
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-21 22:10:32 +0800

    Update A2_2_10_b.m

--- src/A2/A2_2_10_b.m
+++ src/A2/A2_2_10_b.m
@@ -1,14 +1,14 @@
 n = 0:1000;
-x = [1 zeros(1,1000)];
+x = [1 zeros(1, 1000)];
 a1 = 1;
-b1 = [1 zeros(1,999) 0.5];
-y1 = filter(b1,a1,x);
+b1 = [1 zeros(1, 999) 0.5];
+y1 = filter(b1, a1, x);
 
-a2 = [1 zeros(1,999) 0.5];
+a2 = [1 zeros(1, 999) 0.5];
 b2 = 1;
-z1 = filter(b2,a2,y1);
+z1 = filter(b2, a2, y1);
 
 stem(n, z1);
 xlabel('n');
 ylabel('z1');
-title('z[n]+0.5*z-[n-1000] = y[n]');
+title('$z[n] + 0.5 * z[n-1000] = y[n]$');
