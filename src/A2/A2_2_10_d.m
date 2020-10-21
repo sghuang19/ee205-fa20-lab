5e8ae9389ce665810ae4e652880cb01ddcf78326
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-21 16:18:37 +0800

    Update A2_2_10_d.m

--- src/A2/A2_2_10_d.m
+++ src/A2/A2_2_10_d.m
@@ -1,6 +1,19 @@
-load(lineup.mat,y);
+load lineup.mat;
+sound(y,8192)
+
 a = [1 zeros(1,999) 0.5];
 
 r = filter(1,a,y);
-stem(n,r);
+
+subplot(2,1,1);
+plot(y);
+xlabel('n');
+ylabel('Output');
+title('The Origin Sound')
+subplot (2,1,2);
+plot(r);
+xlabel('n');
+ylabel('Output');
+title('The Sound Without Echo')
+
 
