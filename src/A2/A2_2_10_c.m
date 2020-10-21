e255b13dce0d2f74713a83987cc1d345a96c8c25
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-21 22:03:35 +0800

    Update A2_2_10_c.m

--- src/A2/A2_2_10_c.m
+++ src/A2/A2_2_10_c.m
@@ -1,11 +1,11 @@
 n = 0:4000;
-d = [1 zeros(1,4000)];
-a1 = [1 zeros(1,3999) 0.5];
+d = [1 zeros(1, 4000)];
+a1 = [1 zeros(1, 3999) 0.5];
 b1 = 1;
 
-her = filter(b1,a1,d);
+her = filter(b1, a1, d);
 
-stem(n,her);
-xlabel('n');
-ylabel('her');
-title('impulse response for d');
+stem(n, her)
+xlabel('n')
+ylabel('her')
+title('impulse response for d')
