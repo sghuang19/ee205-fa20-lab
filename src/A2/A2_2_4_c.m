93cce693744aa0b7cda31e61d718ec355b83faf3
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-21 22:50:45 +0800

    Update A2_2_4_c.m

--- src/A2/A2_2_4_c.m
+++ src/A2/A2_2_4_c.m
@@ -9,8 +9,10 @@ nw = 0:13;
 w1 = conv(x1, h1) + conv(x1, h2);
 w2 = conv(x1, h3);
 
-subplot(2, 1, 1), stem(nw, w1), axis([0, 13, ylim])
-xlabel('n_w'); ylabel('w_1')
+subplot(2, 1, 1)
+stem(nw, w1)
+xlabel('n_w')
+ylabel('w_1')
 title('w_1[n] = x_1[n] * h_1[n] + x_1[n] * h_2[n]')
 subplot(2, 1, 2)
 stem(nw, w2)
