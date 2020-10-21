71281fc0880c26e3e3266a5dff54528d936ffb30
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-21 11:30:43 +0800

    Update A2_2_10_f_1.m

--- src/A2/A2_2_10_f_1.m
+++ src/A2/A2_2_10_f_1.m
@@ -1,11 +1,8 @@
-% subplot(3, 1, 1)
-% plot(y)
-% subplot(3, 1, 2)
-% plot(y2)
-% subplot(3, 1, 3)
-% plot(y3)
+load lineup.mat
 
+% obtain the autocorrelation of y2
 Rxx = xcorr(y2);
+figure
 plot(Rxx)
 title('The Auotocorrelation of y_2')
 xlabel('index')
@@ -26,14 +23,26 @@ hold on
 plot(p1x, p1y, 'o')
 plot(p2x, p2y, 'o')
 plot(2 * p1x - p2x, p2y, 'o')
+
 % calculate the time difference
 N = abs(p1x - p2x);
 
-% calculate the
+% calculate the attenuation factor alpha
+alp = sqrt(p2y / p1y);
 
-alp = sqrt(p2y / p1y)
-A2 = [1 alp^2];
-B2 = [1];
+% cancel the echo using filter
+A2 = [1 zeros(1, N - 1) alp];
+B2 = 1;
 z = filter(B2, A2, y2);
-sound(z)
+
+% verifying the result
+figure
+subplot(2, 1, 1)
+plot(y2)
+title('y_2[n]')
+subplot(2, 1, 2)
 plot(z)
+title('z[n]')
+ylim([-10, 5])
+
+sound(z)
