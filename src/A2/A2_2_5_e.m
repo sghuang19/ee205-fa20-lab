601197677f9efc1146445edd69cecf7f0e987ced
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-22 08:00:33 +0800

    Update A2_2_5_e.m

--- src/A2/A2_2_5_e.m
+++ src/A2/A2_2_5_e.m
@@ -3,27 +3,22 @@ x1 = ones(1, 20);
 %System1
 a1 = [1 -0.6];
 b1 = [1 0];
-h1 = filter(b1, a1, x);
+s1 = filter(b1, a1, x1);
 %System2
-h2 = zeros(1, 20);
-h2(1) = 1; %cuz h2(i) = 0 if i <1, h2[0] = x[0] = 1, h2[1] = h2[0] + 0;
+s2 = zeros(1, 20);
+s2(1) = 1; %cuz s2(i) = 0 if i <1, s2[0] = x[0] = 1, s2[1] = s2[0] + 0;
 
 for i = 1:19
-    h2(i + 1) = 0.6^i * h2(i) +x(i + 1);
+    s2(i + 1) = 0.6^i * s2(i) +x1(i + 1);
 end
 
-s1 = conv(x1, h1);
-ns1 = 0:38;
-s2 = conv(x1, h2);
-ns2 = 0:38;
-
 subplot(2, 1, 1)
-stem(ns1, s1), axis([0, 38, ylim])
+stem(n, s1)
 xlabel('n');
 ylabel('s1');
-title('s1[n] = x[n]*h1[n]');
+title('s1[n] = 0.6*u[n-1] + u[n]');
 subplot(2, 1, 2)
-stem(ns2, s2), axis([0, 38, ylim])
+stem(n, s2)
 xlabel('n');
 ylabel('s2');
-title('s2[n] = x[n]*h2[n]');
+title('s2[n] = 0.6^n*s2[n-1] + u[n]');
