baf2b55fe3cda01177a7e4f52793092b7d91f996
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-10-22 08:48:18 +0800

    Update A2_2_5_g.m

--- src/A2/A2_2_5_g.m
+++ src/A2/A2_2_5_g.m
@@ -1,13 +1,14 @@
 n = 0:19;
 x = [1 zeros(1, 19)];
 x1 = ones(1, 20);
-%System1
+% System1
 a1 = [1 -0.6];
 b1 = [1 0];
 h1 = filter(b1, a1, x);
-%System2
+% System2
 h2 = zeros(1, 20);
-h2(1) = 1; %cuz h2(i) = 0 if i <1, h2[0] = x[0] = 1, h2[1] = h2[0] + 0;
+h2(1) = 1;
+% h2(i) = 0 if i <1, h2[0] = x[0] = 1, h2[1] = h2[0] + 0;
 
 for i = 1:19
     h2(i + 1) = 0.6^i * h2(i) +x(i + 1);
@@ -28,18 +29,20 @@ end
 subplot(2, 1, 1)
 stem(n, s1, 'o')
 hold on;
-stem(nz1, z1, '*'), axis([0, 19, ylim]);
+stem(nz1, z1, '*')
+axis([0, 19, ylim])
 xlabel('n');
 ylabel('Output');
 title('s1 and z1');
 legend('s1', 'z1');
 
 subplot(2, 1, 2)
-stem(n, s2, 'o'),, axis([0, 19, ylim]);
+stem(n, s2, 'o')
+axis([0, 19, ylim])
 hold on;
 stem(nz2, z2, '*')
 
-xlabel('n');
-ylabel('Output');
-title('s2 and z2');
-legend('s2', 'z2');
+xlabel('n')
+ylabel('Output')
+title('s2 and z2')
+legend('s2', 'z2')
