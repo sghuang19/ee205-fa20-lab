fbbdfc8e811af071ee0cc04ff6736fd9751e43b0
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-29 18:09:38 +0800

    Create A3_3_9_b.m

---
+++ src/A3/A3_3_9_b.m
@@ -0,0 +1,16 @@
+t = linspace(0, 20, 1000);
+
+% square wave
+x2 = cos(t);
+x2(x2 > 0) = ones(size(x2(x2 > 0)));
+x2(x2 < 0) = -ones(size(x2(x2 < 0)));
+
+b = [1 0];
+a = [1 1];
+y2 = lsim(b, a, x2, t);
+
+plot(t, y2)
+xlim([10 20])
+xlabel('t')
+ylabel('Output')
+title('Response of H to x2')
