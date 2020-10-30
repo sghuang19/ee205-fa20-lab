ba80c9e6a5f5068437aabedee3e2b111ae373963
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-31 00:26:45 +0800

    Update A3_3_9_a.m

--- src/A3/A3_3_9_a.m
+++ src/A3/A3_3_9_a.m
@@ -1,17 +1,16 @@
-t = linspace(0,20,1000);
+t = linspace(0, 20, 1000);
 x = cos(t);
 b = [1 0];
 a = [1 1];
 
 y = lsim(b, a, x, t);
 
-plot(t,y)
+plot(t, y)
 xlim([10 20])
 hold on
-plot(t,x)
+plot(t, x)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
 title('Response of H to x')
-legend('y','x');
-
+legend('y', 'x');
