5c52bac4b90bfbc8244fc6f729e664a1b1f7ab79
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-31 10:19:07 +0800

    Create ch3_9_c.m

---
+++ ch3_9_c.m
@@ -0,0 +1,12 @@
+apos_k=[sin(pi/2)/pi,sin(pi)/(2*pi),sin(pi*3/2)/(3*pi),sin(2*pi)/(4*pi),sin(pi*5/2)/(5*pi)];
+aneg_k=[sin(-pi/2)/(-pi),sin(-pi)/(-2*pi),sin(-pi*3/2)/(-3*pi),sin(-2*pi)/(-4*pi),sin(-pi*5/2)/(-5*pi)];
+t=linspace(0,20,1000);
+s1=apos_k(1)*exp(j*t)+aneg_k(1)*exp(-j*t);
+s2=apos_k(2)*exp(j*2*t)+aneg_k(2)*exp(-j*2*t);
+s3=apos_k(3)*exp(j*3*t)+aneg_k(3)*exp(-j*3*t);
+s4=apos_k(4)*exp(j*4*t)+aneg_k(4)*exp(-j*4*t);
+s5=apos_k(5)*exp(j*5*t)+aneg_k(5)*exp(-j*5*t);
+s=real(s1+s2+s3+s4+s5);
+plot(t,s);
+xlabel('t');
+ylabel('Sum of si');
