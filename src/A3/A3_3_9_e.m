3f4f24e65c54eb669e999370bfe24f93dc4dcf84
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-10-31 10:19:29 +0800

    Create A3_3_9_e.m

---
+++ src/A3/A3_3_9_e.m
@@ -0,0 +1,28 @@
+t = linspace(0, 20, 1000);
+x = cos(t);
+b = 1;
+a = [1 1];
+
+% coefficients for x2 CTFS
+apos_k = 2 * [sin(pi / 2) / pi, sin(pi) / (2 * pi), sin(pi * 3/2) / (3 * pi), sin(2 * pi) / (4 * pi), sin(pi * 5/2) / (5 * pi)];
+aneg_k = 2 * [sin(-pi / 2) / (-pi), sin(-pi) / (-2 * pi), sin(-pi * 3/2) / (-3 * pi), sin(-2 * pi) / (-4 * pi), sin(-pi * 5/2) / (-5 * pi)];
+
+s1 = apos_k(1) * exp(j * t) + aneg_k(1) * exp(-j * t);
+s2 = apos_k(2) * exp(j * 2 * t) + aneg_k(2) * exp(-j * 2 * t);
+s3 = apos_k(3) * exp(j * 3 * t) + aneg_k(3) * exp(-j * 3 * t);
+s4 = apos_k(4) * exp(j * 4 * t) + aneg_k(4) * exp(-j * 4 * t);
+s5 = apos_k(5) * exp(j * 5 * t) + aneg_k(5) * exp(-j * 5 * t);
+ssum = real(s1 + s2 + s3 + s4 + s5);
+
+% The two outputs 
+ysum = lsim(b, a, ssum, t);
+y = lsim(b, a, x, t);
+
+%plot
+plot(t, y)
+hold on 
+plot(t, ysum)
+xlabel('t')
+ylabel('Output')
+title('Comparison for Harmonic Components Sum and Original Response')
+legend('Original Output','Harmonic Components Sum')
