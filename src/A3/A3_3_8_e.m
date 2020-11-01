af7203919906380733de7d58a18e88d8ceae4520
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-02 00:01:04 +0800

    Update A3_3_8_e.m

---
+++ src/A3/A3_3_8_e.m
@@ -0,0 +1,31 @@
+k = 0:19;
+% range for input
+n = -20:99;
+a_x = [-1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2 0];
+x_20 = 20 * ifft(a_x);
+x = [x_20 x_20 x_20 x_20 x_20 x_20];
+
+% System1
+a1 = [1 -0.8];
+b1 = 1;
+y1 = filter(b1,a1,x);
+% System2
+a2 = [1 0.8];
+b2 = 1;
+y2 = filter(b2,a2,x);
+
+% plot
+subplot(2,1,1)
+stem(n,y1);
+xlim([0,99])
+xlabel('n')
+ylabel('y1')
+
+subplot(2,1,2)
+stem(n,y2);
+xlim([0,99])
+xlabel('n')
+ylabel('y2')
+
+
+
