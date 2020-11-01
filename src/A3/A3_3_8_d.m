5bb7554e39bad86f89a6cd42e315704e7af21f26
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-02 00:25:48 +0800

    Update A3_3_8_d.m

--- src/A3/A3_3_8_d.m
+++ src/A3/A3_3_8_d.m
@@ -4,7 +4,7 @@ a_x = [-1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2 0];
 x_20 = 20 * ifft(a_x);
 x = [x_20 x_20 x_20 x_20 x_20 x_20];
 
-stem(n, x)
+stem(n, abs(x))
 xlabel('n')
 ylabel('x')
 title('x[n]')
