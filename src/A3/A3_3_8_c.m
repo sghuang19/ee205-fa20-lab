7feb080b043e5e359a5be45272d4e404d8ba6ced
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-02 00:25:45 +0800

    Update A3_3_8_c.m

--- src/A3/A3_3_8_c.m
+++ src/A3/A3_3_8_c.m
@@ -1,8 +1,8 @@
 k = 0:19;
 a_x = [-1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2 0];
-Omega_k = (pi / 10) * k;
+Omega_k = (1 / 10) * k;
 
-stem(Omega_k, a_x)
-xlabel('\omega_k')
+stem(Omega_k, abs(a_x))
+xlabel('\omega_k( \times \pi)') 
 ylabel('Output')
 title('DTFS coefficients of x[n]')
