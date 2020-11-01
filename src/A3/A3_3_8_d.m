8ec72105a0f63b59262617f567f98ed83d7700d1
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-01 09:02:51 +0800

    Create A3_3_8_d.m

---
+++ src/A3/A3_3_8_d.m
@@ -0,0 +1,12 @@
+k = 0:19;
+n = -20:99;
+a_x = [-1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2 0];
+x_20 = 20*ifft(a_x);
+x = [x_20 x_20 x_20 x_20 x_20 x_20];
+
+stem(n,x)
+xlabel('n')
+ylabel('x')
+title('x[n]')
+
+
