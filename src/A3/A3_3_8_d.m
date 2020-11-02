ae917c22dce93f099c50b22965564a2015de926f
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-02 14:11:29 +0800

    Update A3_3_8_d.m

--- src/A3/A3_3_8_d.m
+++ src/A3/A3_3_8_d.m
@@ -1,10 +1,10 @@
 k = 0:19;
 n = -20:99;
-a_x = [-1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2 0];
+a_x = [0 -1/2 zeros(1, 7) 3/4 0 3/4 zeros(1, 7) -1/2];
 x_20 = 20 * ifft(a_x);
 x = [x_20 x_20 x_20 x_20 x_20 x_20];
 
-stem(n, abs(x))
+stem(n, real(x))
 xlabel('n')
 ylabel('x')
 title('x[n]')
