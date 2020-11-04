f4c1e3798da086059df39d5c0c34d700c2533f59
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-04 23:10:06 +0800

    Update A3_3_5_f.m

--- src/A3/A3_3_5_f.m
+++ src/A3/A3_3_5_f.m
@@ -1,23 +1,16 @@
-N3 = 32;
-x3 = [ones(1, 8) zeros(1, 24)];
+N3 = 32
+x3 = [ones(1, 8), zeros(1, 24)];
 a3 = 1 / N3 * fft(x3);
 
-a3_2 = [a3(14:18) zeros(1, 27)];
-a3_8 = [a3(8:24) zeros(1, 15)];
-a3_12 = [a3(4:28) zeros(1, 7)];
-
-x3_2 = zeros(1, 32);
-x3_8 = zeros(1, 32);
-x3_12 = zeros(1, 32);
-x3_all = zeros(1, 32);
-
-r = 0:31;
+a3_2 = [a3(1:3) zeros(1, 27) a3(31:32)];
+a3_8 = [a3(1:9) zeros(1, 15) a3(25:32)];
+a3_12 = [a3(1:13) zeros(1, 7) a3(21:32)];
 
 for n = 1:32
     x3_2(n) = a3(1);
 
     for t = 1:2
-        x3_2(n) = x3_2(n) + a3(t + 1) * exp(j * t * pi * (n - 1) / 16) + conj(a3(t + 1)) * exp((-j) * t * pi * (n - 1) / 16);
+        x3_2(n) = x3_2(n) + a3(t + 1) * exp(j * t * 2 * pi * (n - 1) / 32) + conj(a3(t + 1)) * exp((-j) * t * 2 * pi * (n - 1) / 32);
     end
 
 end
@@ -49,6 +42,7 @@ for n = 1:32
 
 end
 
+r = 0:31;
 subplot(2, 2, 1)
 stem([0:31], real(x3_2))
 xlabel('n')
