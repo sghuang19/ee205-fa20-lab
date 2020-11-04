d94df4fdb5bbadded5b661e9a1ac12513d00fd38
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-05 02:36:06 +0800

    Update A3_3_9_f.m

--- src/A3/A3_3_9_f.m
+++ src/A3/A3_3_9_f.m
@@ -1,23 +1,37 @@
 t = linspace(0, 20, 1000);
 
 % square wave
-x2 = cos(t);
-x2(x2 > 0) = ones(size(x2(x2 > 0)));
-x2(x2 < 0) = -ones(size(x2(x2 < 0)));
+x2 = sign(cos(t));
 
 % coefficients for x2 CTFS
-apos_k = 2 * [sin(pi / 2) / pi, sin(pi) / (2 * pi), sin(pi * 3/2) / (3 * pi), sin(2 * pi) / (4 * pi), sin(pi * 5/2) / (5 * pi)];
-aneg_k = 2 * [sin(-pi / 2) / (-pi), sin(-pi) / (-2 * pi), sin(-pi * 3/2) / (-3 * pi), sin(-2 * pi) / (-4 * pi), sin(-pi * 5/2) / (-5 * pi)];
+for i = 1:5
+    apos_k(i) =  sin(pi*i/2)/(pi*i);
+    aneg_k(i) =  sin(-pi*i/2)/(-pi*i);
+end
 
 s1 = apos_k(1) * exp(j * t) + aneg_k(1) * exp(-j * t);
-s2 = apos_k(2) * exp(j * 2 * t) + aneg_k(2) * exp(-j * 2 * t);
-s3 = apos_k(3) * exp(j * 3 * t) + aneg_k(3) * exp(-j * 3 * t);
-s4 = apos_k(4) * exp(j * 4 * t) + aneg_k(4) * exp(-j * 4 * t);
-s5 = apos_k(5) * exp(j * 5 * t) + aneg_k(5) * exp(-j * 5 * t);
+s2 = apos_k(2) * exp(j * t) + aneg_k(2) * exp(-j * t);
+s3 = apos_k(3) * exp(j * t) + aneg_k(3) * exp(-j * t);
+s4 = apos_k(4) * exp(j * t) + aneg_k(4) * exp(-j * t);
+s5 = apos_k(5) * exp(j * t) + aneg_k(5) * exp(-j * t);
 
 ssum = real(s1 + s2 + s3 + s4 + s5);
 
 % coefficients for y CTFS
+for k=1:10
+    val = ak(k);
+    if ak(k)<0
+       bpos_k = horzcat(apos_k,val);       
+    else
+       bneg_k =horzcat(aneg_k,val);
+    end 
+end
+
+ys1 = real(bpos_k(1)*exp(j*t)+bneg_k(1)*exp(-j*t));
+ys2 = real(bpos_k(2)*exp(j*t)+bneg_k(2)*exp(-j*t));
+ys3 = real(bpos_k(3)*exp(j*t)+bneg_k(3)*exp(-j*t));
+ys4 = real(bpos_k(4)*exp(j*t)+bneg_k(4)*exp(-j*t));
+ys5 = real(bpos_k(5)*exp(j*t)+bneg_k(5)*exp(-j*t));
 
 % Output
 b = 1;
@@ -34,44 +48,44 @@ y = lsim(b, a, x2, t);
 subplot(5, 1, 1)
 plot(t, y1)
 hold on
-plot(t, y)
+plot(t, ys1)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-legend('y1', 'y')
+legend('y1', 'ys1')
 
 subplot(5, 1, 2)
 plot(t, y2)
 hold on
-plot(t, y)
+plot(t, ys2)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-legend('y2', 'y')
+legend('y2', 'ys2')
 
 subplot(5, 1, 3)
 plot(t, y3)
 hold on
-plot(t, y)
+plot(t, ys3)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-legend('y3', 'y')
+legend('y3', 'ys3')
 
 subplot(5, 1, 4)
 plot(t, y4)
 hold on
-plot(t, y)
+plot(t, ys4)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-legend('y4', 'y')
+legend('y4', 'ys4')
 
 subplot(5, 1, 5)
 plot(t, y5)
 hold on
-plot(t, y)
+plot(t, ys5)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-legend('y5', 'y')
+legend('y5', 'ys5')
