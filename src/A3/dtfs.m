2b217c3340e74af85045fa4c4fe2370eb8c66488
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-04 19:21:45 +0800

    Update dtfs.m

--- src/A3/dtfs.m
+++ src/A3/dtfs.m
@@ -1,5 +1,28 @@
-A = [1 -0.8];
-B = [2 0 -1];
+function a = dtfs(x, n_init)
+    N = size(x, 2);
+    a = zeros(1, N);
 
-[H omega] = freqz(B, A, 40, 'whole'); subplot(211); plot(omega, abs(H), '*-'); xlabel('\omega'); ylabel('|H(e^{j\omega} )|'); grid; subplot(212); plot(omega, angle(H), '*-'); xlabel ('\omega');
-ylabel('phase'); grid;
+    for k = 1:N
+
+        for n = 1:N
+            a(k) = a(k) + 1 / N * x(n) * exp(-1j * (k - 1) * 2 * pi / N * (n + n_init - 1));
+        end
+
+    end
+
+end
+
+% A = [1 -0.8];
+% B = [2 0 -1];
+
+% [H, omega] = freqz(B, A, 40, 'whole');
+% subplot(211)
+% plot(omega, abs(H), '*-')
+% xlabel('\omega'); ylabel('|H(e^{j\omega} )|')
+% grid on
+
+% subplot(212)
+% plot(omega, angle(H), '*-')
+% xlabel ('\omega')
+% ylabel('phase')
+% grid on
