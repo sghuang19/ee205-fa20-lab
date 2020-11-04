38863985187041b50decdea6ad0e38f846bb3a1a
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-05 01:30:20 +0800

    Problem i

--- src/A3/A3_3_10_conv.m
+++ src/A3/A3_3_10_conv.m
@@ -67,3 +67,26 @@ hold on
 stem(ny, y_f80c, '*')
 legend('with fft()', 'with conv()')
 title('$$y[n]=\sum_{r=0}^{N-1}x[r]h[n-r]$$, $$N=80$$', 'Interpreter', 'latex')
+%% Problem i
+disp(f40c / f40f)
+disp(f80c / f80f)
+
+N = 160;
+n = 0:N - 1;
+x = 0.9.^n;
+h = 0.5.^n;
+ny = 0:N - 1;
+
+tic
+y = conv([x x], h);
+y_f160c = y(1, 1:N);
+f160c = toc;
+
+tic
+ax = fft(x, N);
+ah = fft(h, N);
+ay = ax .* ah;
+y_f160f = ifft(ay, N);
+f160f = toc;
+
+display(f160c / f160f)
