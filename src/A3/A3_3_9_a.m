0f9cd99fccea2912568069b9e35f7b67e4cb9416
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-05 02:04:04 +0800

    Update A3_3_9_a.m

--- src/A3/A3_3_9_a.m
+++ src/A3/A3_3_9_a.m
@@ -1,6 +1,6 @@
 t = linspace(0, 20, 1000);
 x = cos(t);
-b = [1 0];
+b = 1;
 a = [1 1];
 
 y = lsim(b, a, x, t);
@@ -12,5 +12,5 @@ plot(t, x)
 xlim([10 20])
 xlabel('t')
 ylabel('Output')
-title('Response of H to x')
+title('Response of system $H(s)$ to $x(t)$', 'Interpreter', 'latex')
 legend('y', 'x');
