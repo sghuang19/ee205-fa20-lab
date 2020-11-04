5fc903a5880bce3a3bf61974041e2fae55aec964
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-05 01:30:42 +0800

    Fix time counting

--- src/A3/A3_3_10_conv.m
+++ src/A3/A3_3_10_conv.m
@@ -40,10 +40,12 @@ x = 0.9.^n;
 h = 0.5.^n;
 ny = 0:N - 1;
 
-ax_40 = fft(x, N);
-ah_40 = fft(h, N);
-ay_40 = ax_40 .* ah_40;
-y_f40f = ifft(ay_40, N);
+tic
+ax = fft(x, N);
+ah = fft(h, N);
+ay = ax .* ah;
+y_f40f = ifft(ay, N);
+f40f = toc;
 
 figure
 stem(ny, y_f40f)
@@ -59,10 +61,12 @@ x = 0.9.^n;
 h = 0.5.^n;
 ny = 0:N - 1;
 
-ax_80 = fft(x, N);
-ah_80 = fft(h, N);
-ay_80 = ax_80 .* ah_80;
-y_f80f = ifft(ay_80, N);
+tic
+ax = fft(x, N);
+ah = fft(h, N);
+ay = ax .* ah;
+y_f80f = ifft(ay, N);
+f80f = toc;
 
 figure
 stem(ny, y_f80f)
@@ -70,6 +74,7 @@ hold on
 stem(ny, y_f80c, '*')
 legend('with fft()', 'with conv()')
 title('$$y[n]=\sum_{r=0}^{N-1}x[r]h[n-r]$$, $$N=80$$', 'Interpreter', 'latex')
+
 %% Problem i
 disp(f40c / f40f)
 disp(f80c / f80f)
