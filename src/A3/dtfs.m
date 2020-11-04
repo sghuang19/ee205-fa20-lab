43865e2ddafcd76662aaff171eb838f080d12d6c
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-04 19:29:06 +0800

    Delete useless content

--- src/A3/dtfs.m
+++ src/A3/dtfs.m
@@ -11,18 +11,3 @@ function a = dtfs(x, n_init)
     end
 
 end
-
-% A = [1 -0.8];
-% B = [2 0 -1];
-
-% [H, omega] = freqz(B, A, 40, 'whole');
-% subplot(211)
-% plot(omega, abs(H), '*-')
-% xlabel('\omega'); ylabel('|H(e^{j\omega} )|')
-% grid on
-
-% subplot(212)
-% plot(omega, angle(H), '*-')
-% xlabel ('\omega')
-% ylabel('phase')
-% grid on
