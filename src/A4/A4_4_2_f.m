abadc94b717da06a11d7e351a43a0bad5bec92bd
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-16 19:52:21 +0800

    Update A4_4_2_f.m

--- src/A4/A4_4_2_f.m
+++ src/A4/A4_4_2_f.m
@@ -1,13 +1,13 @@
 tau = 0.01;
 T = 10;
 N = T / tau;
-t = [0:tau:T - 0.01];
+t = (0:tau:T - 0.01);
 x = exp(-2 .* abs(t));
 y = exp(-2 .* abs(t - 5));
 Y = fftshift(tau * fft(y));
 w = -(pi / tau) + (0:N - 1) * (2 * pi / (N * tau));
 
-phase = exp(1j * 5 * w)
+phase = exp(1j * 5 * w);
 X = Y .* phase;
 
 Xanalysis = 1 ./ (2 + 1j * w) + 1 ./ (2 - 1j * w);
