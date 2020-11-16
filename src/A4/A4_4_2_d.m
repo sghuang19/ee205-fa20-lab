3f50760ed0e0be6d815b112536d764522817049f
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-16 19:21:11 +0800

    Create A4_4_2_d.m

---
+++ src/A4/A4_4_2_d.m
@@ -0,0 +1,12 @@
+tau = 0.01;
+T = 10;
+N = T / tau;
+t = [0:tau:T - 0.01];
+x = exp(-2 .* abs(t));
+y = exp(-2 .* abs(t - 5));
+Y = fftshift(tau * fft(y));
+w = -(pi / tau) + (0:N - 1) * (2 * pi / (N * tau));
+
+plot(w,real(Y))
+xlabel('\omega')
+ylabel('Y(j\omega)')
