a4202d9f5ad0792112c66a9bae8816227f7c697c
AutomataLo <71057284+AutomataLo@users.noreply.github.com>
2020-11-17 15:47:10 +0800

    Update A4_4_5_a.m

--- src/A4/A4_4_5_a.m
+++ src/A4/A4_4_5_a.m
@@ -1,13 +1,13 @@
-b1 = [2 -1];
+b1 = [1 -2];
 a1 = [1 3/2 1/2];
 H1 = freqs(b1, a1);
 subplot(2, 1, 1)
 semilogy(abs(H1))
 title('Magnitude of H1(j\omega)')
-xlabel('Frequaency ( rad/s )')
+xlabel('Frequency ( rad/s )')
 ylabel('Magnitude')
 subplot(2, 1, 2)
 plot(unwrap(angle(H1)))
 title('Phase of H1(j\omega)')
-xlabel('Frequaency ( rad/s )')
+xlabel('Frequency ( rad/s )')
 ylabel('Phase')
