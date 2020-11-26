e403b39b442ba0ce8fc09e10f8634cd15dfc2f2a
SamuelHuang2019 <58419736+SamuelHuang2019@users.noreply.github.com>
2020-11-26 11:27:08 +0800

    Update A5.m

--- src/A5/A5.m
+++ src/A5/A5.m
@@ -1,21 +1,22 @@
 close all
 
 %% part 1
-[x, fs] = audioread('C_01_01.wav');
+[x, Fs] = audioread('C_01_01.wav');
 sig = repmat(x, 5, 1);
-% sound(sig, fs)
+% Fs = Fs;
+% sound(sig, Fs)
 
-[Pxx, w] = periodogram(sig, [], 512, fs);
-plot(w, Pxx)
+[pxx, w] = periodogram(sig, [], 512, Fs);
+plot(w, pxx)
 title('power spectrum density of the speech signal')
 xlabel('frequency')
 ylabel('power spectral density')
 
-b = fir2(3000, w / (fs / 2), sqrt(Pxx / max(Pxx)));
+b = fir2(3000, w / (Fs / 2), sqrt(pxx / max(pxx)));
 [N, ~] = size(sig);
 noise = 1 - 2 * rand(N, 1);
 
-[Pxx, w] = periodogram(noise);
+[pxx, w] = periodogram(noise);
 
 %% part 2
 ssn = filter(b, 1, noise);
@@ -29,21 +30,21 @@ norm(y)
 y = abs(y);
 figure
 
-[b, a] = butter(2, 100 / (fs / 2));
+[b, a] = butter(2, 100 / (Fs / 2));
 env = filter(b, a, y);
 subplot(3, 1, 1)
 plot(env)
 title('f_{cut} = 100Hz')
 hold on
 
-[b, a] = butter(2, 200 / (fs / 2));
+[b, a] = butter(2, 200 / (Fs / 2));
 env = filter(b, a, y);
 subplot(3, 1, 2)
 plot(env)
 title('f_{cut} = 200Hz')
 hold on
 
-[b, a] = butter(2, 300 / (fs / 2));
+[b, a] = butter(2, 300 / (Fs / 2));
 subplot(3, 1, 3)
 env = filter(b, a, y);
 plot(env)
@@ -51,15 +52,17 @@ title('f_{cut} = 300Hz')
 
 % ====================================
 
-[b, a] = butter(2, 200 / (fs / 2));
+[b, a] = butter(2, 200 / (Fs / 2));
 env = filter(b, a, y);
 subplot(2, 1, 1)
 plot(env)
 title('2nd-order')
 hold on
 
-[b, a] = butter(6, 200 / (fs / 2));
+[b, a] = butter(6, 200 / (Fs / 2));
 env = filter(b, a, y);
 subplot(2, 1, 2)
 plot(env)
 title('6th-order')
+
+% ===============================
